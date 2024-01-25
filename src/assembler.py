9b7d1dc2bd5abdda1df81e50cdd728fb4b854b72
an0nymoos3 <moosely123@gmail.com>
2024-01-25 17:36:17 +0100

    updated jumps

--- src/assembler.py
+++ src/assembler.py
@@ -239,16 +239,11 @@ def calc_branch_jmps(program) -> list:
         if line[0] == "BNE" or line[0] == "BRA" or line[0] == "BEQ" or line[0] == "BGE":
             if "#" in line[1]:
                 branch_name = line[1].strip()
-                branch_line_num = find_branch(program, branch_name) - 2
-
-                if i < branch_line_num:
-                    rel_jump = branch_line_num - i
-                else:
-                    rel_jump = 255 - (i - branch_line_num)
+                branch_line_num = find_branch(program, branch_name)
 
                 # Replace name with an ADR looking hex number, 
                 # so that it gets parsed to binary later in assember
-                line[1] = "$" + dec_to_hex(str(rel_jump))
+                line[1] = "$" + dec_to_hex(str(branch_line_num))
 
     return program
 
